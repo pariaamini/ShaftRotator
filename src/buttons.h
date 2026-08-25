97c6e4324dd62304fa3df6bc83f446901d8aba13
paria amini <paria.amini@gmail.com>
2026-08-25 09:21:59 -0600

    only main+constants

--- src/buttons.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <Arduino.h>
-
-// Set up button callbacks and press timings.
-void setupButtons();
-
-// Check each button for presses, releases, and long presses.
-void updateButtons();
--- src/main.ino
+++ src/main.ino
@@ -1,15 +1,9 @@
-#include "motor.h"
-#include "buttons.h"
 
 void setup()
 {
-  setupButtons();
-  setupMotor();
 }
 
 // Continuously runs and checks button outputs
 void loop()
 {
-  updateButtons();
-  updateMotor();
 }
